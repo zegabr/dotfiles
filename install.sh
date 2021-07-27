a6143bb3a6398b8e94149fca3205d206e8d21b74
Jose <jgsp2@cin.ufpe.br>
2021-07-26 22:27:54 -0300

    remove simlink before adding

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
-stow --verbose=2 -t ~/ vim
+stow -D --verbose=2 vim # remove simlink
+stow --verbose=2 -t ~/ vim # add simlink
+
+stow -D --verbose=2 bash
 stow --verbose=2 -t ~/ bash
+
+stow -D --verbose=2 zsh
 stow --verbose=2 -t ~/ zsh; source ~/.zshrc
