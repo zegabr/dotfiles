4fc812458c6bf3ed2e33800da373d1c032750917
zegabr <jgsp2@cin.ufpe.br>
2021-11-16 16:34:14 -0300

    fix go language server

--- nvim/.config/nvim/lua/zegabr/lsp.lua
+++ nvim/.config/nvim/lua/zegabr/lsp.lua
@@ -158,6 +158,7 @@ require'lspconfig'.clangd.setup {
 require'lspconfig'.dockerls.setup{}
 
 -- go
+-- sudo snap install gopls
 require'lspconfig'.gopls.setup({
     cmd = {"gopls", "serve"},
     settings = {
@@ -170,20 +171,54 @@ require'lspconfig'.gopls.setup({
     },
 })
 
+function GOIMPORTS(timeout_ms)
+    local context = { only = { "source.organizeImports" } }
+    vim.validate { context = { context, "t", true } }
+
+    local params = vim.lsp.util.make_range_params()
+    params.context = context
+
+    -- See the implementation of the textDocument/codeAction callback
+    -- (lua/vim/lsp/handler.lua) for how to do this properly.
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+    if not result or next(result) == nil then return end
+    local actions = result[1].result
+    if not actions then return end
+    local action = actions[1]
+
+    -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+    -- is a CodeAction, it can have either an edit, a command or both. Edits
+    -- should be executed first.
+    if action.edit or type(action.command) == "table" then
+        if action.edit then
+            vim.lsp.util.apply_workspace_edit(action.edit)
+        end
+        if type(action.command) == "table" then
+            vim.lsp.buf.execute_command(action.command)
+        end
+    else
+        vim.lsp.buf.execute_command(action)
+    end
+end
+
+vim.api.nvim_command([[
+autocmd BufWritePre *.go lua GOIMPORTS(1000)
+]])
+
 -- html css json eslint
 -- npm i -g vscode-langservers-extracted
 updated_capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 require'lspconfig'.html.setup {
-  capabilities = updated_capabilities,
+    capabilities = updated_capabilities,
 }
 
 require'lspconfig'.cssls.setup {
-  capabilities = updated_capabilities,
+    capabilities = updated_capabilities,
 }
 
 require'lspconfig'.jsonls.setup {
-  capabilities = updated_capabilities,
+    capabilities = updated_capabilities,
 }
 
 require'lspconfig'.eslint.setup{}
