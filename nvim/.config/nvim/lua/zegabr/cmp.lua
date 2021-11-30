329b1d3710bfbfbe726d6a88ce680e224196d25c
zegabr <jgsp2@cin.ufpe.br>
2021-11-30 15:04:39 -0300

    finnish tabnine setup

--- nvim/.config/nvim/lua/zegabr/cmp.lua
+++ nvim/.config/nvim/lua/zegabr/cmp.lua
@@ -77,3 +77,12 @@ cmp.setup {
         end
     },
 }
+
+local tabnine = require('cmp_tabnine.config')
+tabnine:setup({
+    max_lines = 1000,
+    max_num_results = 20,
+    sort = true,
+    run_on_every_keystroke = true,
+    snippet_placeholder = '..',
+})
