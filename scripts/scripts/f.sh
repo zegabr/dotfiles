9d614d40f2ec0d9374a461c712ea2b85b23e4b3e
zegabr <jgsp2@cin.ufpe.br>
2022-05-12 19:34:39 -0300

    add script to install font

---
+++ scripts/scripts/f.sh
@@ -0,0 +1,6 @@
+#Font installation
+cd /usr/share/fonts/
+sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+sudo unzip FiraCode.zip
+sudo rm -rf FiraCode.zip
+cd -
