39d2ac64487b746f1d977234a52f6319af741b54
zegabr <jgsp2@cin.ufpe.br>
2022-06-15 18:58:31 -0300

    fix multi selection telescope https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-889122232

--- nvim/.config/nvim/lua/zegabr/telescope.lua
+++ nvim/.config/nvim/lua/zegabr/telescope.lua
@@ -26,6 +26,48 @@ telescope.setup {
     }
 }
 
+
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+
+local custom_actions = {}
+function custom_actions.fzf_multi_select(prompt_bufnr)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local num_selections = table.getn(picker:get_multi_selection())
+
+  if num_selections > 1 then
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    for _, entry in ipairs(picker:get_multi_selection()) do
+      vim.cmd(string.format("%s %s", ":e!", entry.value))
+    end
+    vim.cmd('stopinsert')
+  else
+    actions.file_edit(prompt_bufnr)
+  end
+end
+
+require('telescope').setup {
+  defaults = {
+    file_ignore_patterns = { "node_modules", ".git" },
+    mappings = {
+      i = {
+        ['<esc>'] = actions.close,
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous,
+        ['<tab>'] = actions.toggle_selection + actions.move_selection_previous,
+        ['<s-tab>'] = actions.toggle_selection + actions.move_selection_next,
+        ['<cr>'] = custom_actions.fzf_multi_select,
+      },
+      n = {
+        ['<esc>'] = actions.close,
+        ['<tab>'] = actions.toggle_selection + actions.move_selection_previous,
+        ['<s-tab>'] = actions.toggle_selection + actions.move_selection_next,
+        ['<cr>'] = custom_actions.fzf_multi_select
+      }
+    },
+  }
+}
+
 local M = {}
 
 M.project_files = function()
@@ -35,3 +77,4 @@ M.project_files = function()
 end
 
 return M
+
