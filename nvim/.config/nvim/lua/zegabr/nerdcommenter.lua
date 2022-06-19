c8d6e88734eaddfc218e5d2c6705e6a049c0e265
zegabr <jgsp2@cin.ufpe.br>
2022-06-18 23:47:00 -0300

    nerdcommenter.vim -> nerdcommenter.lua

--- nvim/.config/nvim/lua/zegabr/nerdcommenter.lua
+++ nvim/.config/nvim/lua/zegabr/nerdcommenter.lua
@@ -1,5 +1,7 @@
+vim.cmd([[
 filetype plugin on
 let g:NERDCreateDefaultMappings = 0
 imap <C-_> <ESC><Plug>NERDCommenterToggle
 nmap <C-_> <Plug>NERDCommenterToggle
 vmap <C-_> <Plug>NERDCommenterToggle<CR>gv
+]])
