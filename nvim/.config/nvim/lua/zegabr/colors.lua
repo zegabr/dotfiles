fbaab70473d63dae60373d69412fd3cc7765f2b7
zegabr <jgsp2@cin.ufpe.br>
2022-06-18 23:15:23 -0300

    icolors.vim -> colors.lua

---
+++ nvim/.config/nvim/lua/zegabr/colors.lua
@@ -0,0 +1,7 @@
+vim.opt.termguicolors = true
+vim.opt.showmode = false
+vim.cmd "colorscheme gruvbox"
+vim.g.airline_theme="deus"
+vim.cmd "let g:airline#extensions#tabline#enabled=1"
+vim.cmd "let g:airline#extensions#tabline#fnamemode=':t'"
+vim.opt.background="dark"
--- nvim/.config/nvim/plugin/colors.vim
+++
@@ -1,7 +0,0 @@
-set termguicolors
-set noshowmode
-colorscheme gruvbox
-let g:airline_theme='deus'
-let g:airline#extensions#tabline#enabled=1
-let g:airline#extensions#tabline#fnamemode=':t'
-set background=dark
