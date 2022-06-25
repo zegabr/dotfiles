703958e7262d7788a27d39ac69bb07ae86e3f461
zegabr <jgsp2@cin.ufpe.br>
2022-06-25 20:42:48 -0300

    update sets considering :h nvim-defaults

--- nvim/.config/nvim/lua/zegabr/sets.lua
+++ nvim/.config/nvim/lua/zegabr/sets.lua
@@ -1,31 +1,23 @@
+
 vim.cmd([[
 " ============SETS (and passive mappings)============
-syntax on
-filetype on
 filetype plugin on
 set modifiable " make netrw be able to modify directories
 set grepprg=rg\ --vimgrep " use rg as default grepper
 set updatetime=100
-set encoding=utf-8
 set guicursor=
 set exrc "also source vimrcs inside directory of file
-set nocompatible
-set showcmd
 set showmatch " show matching open-closing symbol
 set splitright splitbelow
 set title
 set scrolloff=8
 set list
-set listchars=tab:>\ ,trail:-
 
 set number " show line numbers
 set relativenumber "line numbers moving relatively
-set noerrorbells
 set nowrap
 set cursorline cursorcolumn
 
-set nohlsearch "search highlight off
-set incsearch "cursor moves as soon as typing search
 set ignorecase "search ignore cases
 set smartcase "search start to not ignore cases if search for uppercase letter
 
