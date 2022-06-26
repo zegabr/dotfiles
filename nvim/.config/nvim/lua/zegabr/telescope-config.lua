1d1463d1c69643e5a08483a28f61815b05037315
zegabr <jgsp2@cin.ufpe.br>
2022-06-26 17:13:24 -0300

    fix search word in project

--- nvim/.config/nvim/lua/zegabr/telescope-config.lua
+++ nvim/.config/nvim/lua/zegabr/telescope-config.lua
@@ -5,20 +5,20 @@ local custom_actions = {}
 telescope.setup {
     defaults = {
         mappings = {
-          i = {
-            ['<esc>'] = actions.close,
-            ['<C-j>'] = actions.move_selection_next,
-            ['<C-k>'] = actions.move_selection_previous,
-            ['<tab>'] = actions.toggle_selection + actions.move_selection_previous,
-            ['<s-tab>'] = actions.toggle_selection + actions.move_selection_next,
-            ['<cr>'] = custom_actions.fzf_multi_select,
-          },
-          n = {
-            ['<esc>'] = actions.close,
-            ['<tab>'] = actions.toggle_selection + actions.move_selection_previous,
-            ['<s-tab>'] = actions.toggle_selection + actions.move_selection_next,
-            ['<cr>'] = custom_actions.fzf_multi_select
-          }
+            i = {
+                ['<esc>'] = actions.close,
+                ['<C-j>'] = actions.move_selection_next,
+                ['<C-k>'] = actions.move_selection_previous,
+                ['<tab>'] = actions.toggle_selection + actions.move_selection_previous,
+                ['<s-tab>'] = actions.toggle_selection + actions.move_selection_next,
+                ['<cr>'] = custom_actions.fzf_multi_select,
+            },
+            n = {
+                ['<esc>'] = actions.close,
+                ['<tab>'] = actions.toggle_selection + actions.move_selection_previous,
+                ['<s-tab>'] = actions.toggle_selection + actions.move_selection_next,
+                ['<cr>'] = custom_actions.fzf_multi_select
+            }
         },
         vimgrep_arguments = { --TODO: fix this to add regex?? this is not finding any "require(" (only "require\("), i shouldn't have to escape the parentesis
             "rg",
@@ -47,13 +47,18 @@ local keymap = require('zegabr.keymap')
 local nnoremap = keymap.nnoremap
 
 local project_files = function()
-  local opts = {hidden=true} -- define here if you want to define something
-  local ok = pcall(builtin.git_files, opts)
-  if not ok then builtin.find_files(opts) end
+    local opts = {hidden=true} -- define here if you want to define something
+    local ok = pcall(builtin.git_files, opts)
+    if not ok then builtin.find_files(opts) end
 end
 
 
- nnoremap("<C-p>", function() project_files() end)
- nnoremap("<C-f>", function() builtin.live_grep() end)
- nnoremap("<C-b>", function() builtin.buffers() end)
- nnoremap("<leader>gb", function() builtin.git_branches() end)
+nnoremap("<C-p>", function() project_files() end)
+nnoremap("<C-f>", function() builtin.grep_string({
+    shorten_path = true,
+    word_match = "-w",
+    only_sort_text = true,
+    search = ''
+}) end)
+nnoremap("<C-b>", function() builtin.buffers() end)
+nnoremap("<leader>gb", function() builtin.git_branches() end)
