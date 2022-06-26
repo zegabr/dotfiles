2a8370e13916d90e6fcad4bf2fc947950e34fe6f
zegabr <jgsp2@cin.ufpe.br>
2022-06-26 19:13:00 -0300

    undotree almost lua

--- nvim/.config/nvim/lua/zegabr/undotree.lua
+++ nvim/.config/nvim/lua/zegabr/undotree.lua
@@ -1,26 +1,27 @@
+
 vim.cmd([[
-set noswapfile
-set nobackup
-set nowritebackup
-set undodir=~/.undodir
-set undofile
-nnoremap <F5> :UndotreeToggle<CR>
 let g:undotree_WindowLayout = 2
 if has("persistent_undo")
-    let target_path = expand('~/.undodir')
-    " create the directory and any parent directories
-    " if the location does not exist.
-    if !isdirectory(target_path)
-        call mkdir(target_path, "p", 0700)
-    endif
-    let &undodir=target_path
+    set undodir=$HOME."/.undodir"
     set undofile
 endif
-
-" undo break points
-inoremap , ,<c-g>u
-inoremap ? ?<c-g>u
-inoremap ! !<c-g>u
-inoremap . .<c-g>u
-inoremap ; ;<c-g>u
 ]])
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.undodir"
+vim.opt.undofile = true
+
+local keymap = require('zegabr.keymap')
+local nnoremap = keymap.nnoremap
+local inoremap = keymap.inoremap
+
+-- toggle
+nnoremap("<F5>", ":UndotreeToggle<CR>")
+
+--" undo break points
+inoremap(",", ",<c-g>u")
+inoremap("?", "?<c-g>u")
+inoremap("!", "!<c-g>u")
+inoremap(".", ".<c-g>u")
+inoremap(";", ";<c-g>u")
