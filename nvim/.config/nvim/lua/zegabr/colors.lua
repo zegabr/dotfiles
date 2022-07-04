61e54ea2e4bf9f932a2948b78d8418e30dddc089
zegabr <jgsp2@cin.ufpe.br>
2022-07-03 22:53:25 -0300

    remove unused defaults for lualine and nvim-tree

--- nvim/.config/nvim/lua/zegabr/colors.lua
+++ nvim/.config/nvim/lua/zegabr/colors.lua
@@ -20,28 +20,6 @@ vim.cmd[[ colorscheme gruvbox ]]
 
 require('lualine').setup({
     options = { theme = 'gruvbox' },
-    tabs = {
-        sections = {
-            lualine_a = {
-                {
-                    'tabs',
-                    max_length = vim.o.columns / 3, -- Maximum width of tabs component.
-                    -- Note:
-                    -- It can also be a function that returns
-                    -- the value of `max_length` dynamically.
-                    mode = 0, -- 0: Shows tab_nr
-                    -- 1: Shows tab_name
-                    -- 2: Shows tab_nr + tab_name
-
-                    tabs_color = {
-                        -- Same values as the general color option can be used here.
-                        active = 'lualine_{section}_normal',     -- Color for active tab.
-                        inactive = 'lualine_{section}_inactive', -- Color for inactive tab.
-                    },
-                }
-            }
-        }
-    }
 })
 
 require('tabline').setup {
