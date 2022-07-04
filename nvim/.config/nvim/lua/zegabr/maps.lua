02ff35ca8360704d1502fb6379884c655aaf011e
zegabr <jgsp2@cin.ufpe.br>
2022-07-03 21:01:40 -0300

    luafile not source

--- nvim/.config/nvim/lua/zegabr/maps.lua
+++ nvim/.config/nvim/lua/zegabr/maps.lua
@@ -44,7 +44,7 @@ nnoremap("<C-l>", ":cnext<CR>zz")
 nnoremap("<C-q>", ":cclose<CR>zz")
 
 --" below command is for vimrc testing while editing it
-nnoremap("<Leader><CR>", ":so ~/.config/nvim/init.lua<CR>")
+nnoremap("<Leader><CR>", ":luafile ~/.config/nvim/init.lua<CR>")
 
 --" ============SETS (and passive mappings)============
 
