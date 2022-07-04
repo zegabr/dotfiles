bf63f524f883a2a44b64c055393702fc3cf8295a
zegabr <jgsp2@cin.ufpe.br>
2022-07-03 22:29:26 -0300

    simplify modularization (2 init.lua -> 1 init.lua)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,21 @@
-require("zegabr")
+-- basic
+require('zegabr.maps')
+require('zegabr.sets')
+
+-- plugins
+require('zegabr.plugins')
+
+-- colors
+require('zegabr.treesitter')
+require('zegabr.colors')
+
+-- utils
+require('zegabr.gitutils')
+
+-- ide features
+require('zegabr.undotree') -- this does not exits in vscode u.u
+require('zegabr.telescope')
+require('zegabr.lsp')
+require('zegabr.cmp')
+require('zegabr.nvim-tree')
+require('zegabr.wilder')
