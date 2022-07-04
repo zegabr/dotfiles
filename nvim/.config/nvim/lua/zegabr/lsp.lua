33bf163e392c705212b2032618a98df3b364b1e9
zegabr <jgsp2@cin.ufpe.br>
2022-07-03 23:03:25 -0300

    fix lua go to definition problem

--- nvim/.config/nvim/lua/zegabr/lsp.lua
+++ nvim/.config/nvim/lua/zegabr/lsp.lua
@@ -46,10 +46,10 @@ local lua_settings = {
             -- Get the language server to recognize the `vim` global
             globals = {'vim'},
         },
-        workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-        },
+        -- workspace = {
+        --     -- Make the server aware of Neovim runtime files
+        --     library = vim.api.nvim_get_runtime_file("", true),
+        -- },
         telemetry = {
             enable = false,
         }
