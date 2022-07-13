7853d70103c9c39bb5ca84afd2b5bbc87794ffb4
zegabr <jgsp2@cin.ufpe.br>
2022-07-13 20:48:25 -0300

    update nvim wilder

--- nvim/.config/nvim/lua/zegabr/wilder.lua
+++ nvim/.config/nvim/lua/zegabr/wilder.lua
@@ -1,39 +1,31 @@
 local wilder = require('wilder')
-
 wilder.setup({modes = {':', '/', '?'}})
+-- Disable Python remote plugin
+wilder.set_option('use_python_remote_plugin', 0)
 
 wilder.set_option('pipeline', {
-    wilder.branch(
-        wilder.cmdline_pipeline({
-            language = 'python',
-            fuzzy = 1,
-            fuzzy_filter = wilder.lua_fzy_filter(),
-        }),
-        wilder.python_search_pipeline({
-            -- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
-            pattern = wilder.python_fuzzy_pattern(),
-            -- omit to get results in the order they appear in the buffer
-            sorter = wilder.python_difflib_sorter(),
-            -- can be set to 're2' for performance, requires pyre2 to be installed
-            -- see :h wilder#python_search() for more details
-            engine = 're2',
-        })
-    )
+  wilder.branch(
+    wilder.cmdline_pipeline({
+      fuzzy = 1,
+      fuzzy_filter = wilder.lua_fzy_filter(),
+    }),
+    wilder.vim_search_pipeline()
+  )
 })
 
 wilder.set_option('renderer', wilder.renderer_mux({
-    [':'] = wilder.popupmenu_renderer({
-        highlighter = wilder.lua_fzy_highlighter(),
-        left = {
-            ' ',
-            wilder.popupmenu_devicons()
-        },
-        right = {
-            ' ',
-            wilder.popupmenu_scrollbar()
-        },
-    }),
-    ['/'] = wilder.wildmenu_renderer({
-        highlighter = wilder.lua_fzy_highlighter(),
-    }),
+  [':'] = wilder.popupmenu_renderer({
+    highlighter = wilder.lua_fzy_highlighter(),
+    left = {
+      ' ',
+      wilder.popupmenu_devicons()
+    },
+    right = {
+      ' ',
+      wilder.popupmenu_scrollbar()
+    },
+  }),
+  ['/'] = wilder.wildmenu_renderer({
+    highlighter = wilder.lua_fzy_highlighter(),
+  }),
 }))
