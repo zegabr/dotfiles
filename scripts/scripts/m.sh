26ee0d42477597e4462bf4182dff49b92e35c9fc
José Gabriel <jgsp2@cin.ufpe.br>
2022-07-25 23:02:28 -0700

    d.sh does not exists??

--- scripts/scripts/m.sh
+++ scripts/scripts/m.sh
@@ -9,5 +9,5 @@ bash g.sh # gnome
 bash ssh.sh # setup ssh
 source z.sh # add updates zshrc, has to be called before dotfiles
 rm ~/.zshrc
-source d.sh # remove zshrc and create a symlink to dotfile zshrc
+# source d.sh # remove zshrc and create a symlink to dotfile zshrc
 
