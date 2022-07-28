b8064af26fc72f84be00dce54e8d485bea3b4a03
zegabr <jgsp2@cin.ufpe.br>
2022-07-27 19:27:38 -0700

    could not fully automatize zsh installation

--- scripts/scripts/z.sh
+++ scripts/scripts/z.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 # Z-Shell installation (with oh-my-zsh)
-if ! [ -x "$(command -v zsh)" ]; then
-    rm -rf ~/.oh-my-zsh
-    sudo apt install zsh -y
-    sudo apt install curl
-    chsh -s $(which zsh) #change default terminal to be zsh
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+rm -rf ~/.oh-my-zsh
+
+sudo apt install zsh curl -y
+chsh -s $(which zsh) #change default terminal to be zsh
+
+echo """
+
+======================== PRESS CTRL D AFTER TERMINAL CHANGES TO CONTINUE ==================
+
+"""
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #clone plugins as such
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-source ~/.zshrc
 echo "PLS OPEN ANOTHER TERMINAL FOR CHANGES TAKE EFFECT "
-exit
