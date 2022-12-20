b4afc533ab22598fe0de3961ebffea54620c3a30
zegabr <jgsp2@cin.ufpe.br>
2022-12-19 20:06:39 -0800

    fix autocompletion tab behavior

--- nvim/.config/nvim/lua/zegabr/lsp.lua
+++ nvim/.config/nvim/lua/zegabr/lsp.lua
@@ -16,11 +16,9 @@ lsp.set_preferences({
 })
 
 local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local luasnip = require('luasnip')
 lsp.setup_nvim_cmp({
     mapping = lsp.defaults.cmp_mappings({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ["<C-Space>"] = cmp.mapping.complete(),
         ['<C-j>'] = cmp.mapping.scroll_docs(4),
         ['<C-k>'] = cmp.mapping.scroll_docs(-4),
@@ -28,12 +26,39 @@ lsp.setup_nvim_cmp({
         ['<CR>'] = cmp.mapping(
             cmp.mapping.confirm {
                 behavior = cmp.ConfirmBehavior.Replace,
-                select = false,
+                select = true,
             },
             { "i", "c" }
         ),
-    })
-
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    }),
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        -- TODO: add others
+    },
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
 })
 
 lsp.on_attach(function(_, bufnr)
