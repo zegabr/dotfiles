24f5b84795a08fd8dac97d31d92cdaa8e3798ebb
zegabr <jgsp2@cin.ufpe.br>
2023-02-24 00:09:24 -0300

    update app instalation script

--- scripts/scripts/stuff/apps.sh
+++ scripts/scripts/stuff/apps.sh
@@ -2,28 +2,12 @@
 mkdir ~/Downloads
 cd ~/Downloads
 
-sudo apt update
-
-# remove node npm and nodejs
-sudo apt remove nodejs
-sudo apt remove npm
-sudo apt update
-sudo rm -rf $(which node)
-sudo rm -rf $(which nodejs)
-sudo rm -rf $(which npm)
-# install nvm to install updated node for nvim lsp
-# https://www.freecodecamp.org/news/how-to-install-node-js-on-ubuntu-and-update-npm-to-the-latest-version/
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-
-# rust
-sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source "$HOME/.cargo/env"
-rustc --version
-rustup component add rust-docs
-rustup component add rust-analyzer
+sudo apt update -y
 
 sudo apt install \
 tree \
+flameshot \
+gawk \
 git \
 build-essential \
 wget \
@@ -45,6 +29,13 @@ sudo snap install spotify
 sudo snap install libreoffice
 sudo snap install slack
 
+# rust
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+rustc --version
+rustup component add rust-docs
+rustup component add rust-analyzer
+
 # DOCKER
 read -p "Type y if you want Docker to be installed (y/n): " ans
 if [ "$ans" == "y" ]; then
@@ -57,13 +48,13 @@ if [ "$ans" == "y" ]; then
     sudo chmod +x /usr/local/bin/docker-compose
 fi
 
-# Ripgrep
-read -p "Type y if you want Ripgrep to be installed (y/n): " ans
-if [ "$ans" == "y" ]; then
+# Ripgrep (I always want this for telescope plugin)
+# read -p "Type y if you want Ripgrep to be installed (y/n): " ans
+# if [ "$ans" == "y" ]; then
     curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb
     sudo dpkg -i ripgrep_12.1.1_amd64.deb
     rm ripgrep_12.1.1_amd64.deb
-fi
+# fi
 
 # GOOGLE CHROME
 read -p "Type y if you want Google Chrome to be installed (y/n): " ans
