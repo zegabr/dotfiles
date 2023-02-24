dc12295efc14f4c04411e3b2044e51df5b8eade4
zegabr <jgsp2@cin.ufpe.br>
2023-02-24 00:09:06 -0300

    isolate nvm instalation

--- scripts/scripts/m.sh
+++ scripts/scripts/m.sh
@@ -3,12 +3,10 @@
 
 bash setup.sh #install apps and docker
 
-# TODO: test this
-source ~/.bashrc
 source n.sh
 
 source v.sh # install nvim building from source
 bash t.sh # install tmux
 bash ssh.sh # setup ssh
 source d.sh # simlinks things
-
+source ~/.bash_aliases
--- scripts/scripts/n.sh
+++ scripts/scripts/n.sh
@@ -1,6 +1,18 @@
 # UPDATE NPM VERSION FOR NVIM
-# TODO(important): this must be run after apps.sh + terminal reset
-# TODO: check if this works and if comment above is garbage
+# remove node npm and nodejs
+sudo apt remove nodejs -y
+sudo apt remove npm -y
+sudo apt update -y
+sudo rm -rf $(which node)
+sudo rm -rf $(which nodejs)
+sudo rm -rf $(which npm)
+# install nvm to install updated node for nvim lsp
+# https://www.freecodecamp.org/news/how-to-install-node-js-on-ubuntu-and-update-npm-to-the-latest-version/
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# TODO: after above, need to run below in a new terminal (why source ~/.bashrc isn't enough?)
+
+source ~/.bashrc
 nvm install --latest-npm
 npm i -g yarn
 nvm ls
