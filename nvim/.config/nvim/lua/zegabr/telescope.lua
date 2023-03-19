44b1f612d45e08b6669a4802fe491877af23af50
zegabr <jgsp2@cin.ufpe.br>
2023-03-18 21:10:56 -0300

    add telescope multiselect workaround

--- nvim/.config/nvim/lua/zegabr/telescope.lua
+++ nvim/.config/nvim/lua/zegabr/telescope.lua
@@ -1,18 +1,26 @@
 local telescope = require('telescope')
 local actions = require('telescope.actions')
+local action_state = require("telescope.actions.state")
+
+local mymappings = {
+    ['<C-j>'] = actions.move_selection_next,
+    ['<C-k>'] = actions.move_selection_previous,
+    ['<tab>'] = actions.toggle_selection,
+    ['<cr>'] = function(pb) -- TODO: change this to file edit all when this issue is closed https://github.com/nvim-telescope/telescope.nvim/issues/416
+        local picker = action_state.get_current_picker(pb)
+        local multi = picker:get_multi_selection()
+        actions.select_default(pb) -- the normal enter behaviour
+        for _, j in pairs(multi) do
+            if j.path ~= nil then  -- is it a file -> open it as well:
+                vim.cmd(string.format("%s %s", "edit", j.path))
+            end
+        end
+    end,
+}
 
 telescope.setup {
     defaults = {
-        mappings = {
-            i = {
-                ['<esc>'] = actions.close,
-                ['<C-j>'] = actions.move_selection_next,
-                ['<C-k>'] = actions.move_selection_previous,
-                ['<tab>'] = actions.toggle_selection,
-                ['<cr>'] = actions.file_edit, -- TODO: change this to file edit all when this issue is closed https://github.com/nvim-telescope/telescope.nvim/issues/416
-            },
-        },
-
+        mappings = { i = mymappings, n = mymappings, },
         vimgrep_arguments = {
             "rg",
             "--color=never",
@@ -47,19 +55,19 @@ local function project_files()
 end
 
 vim.keymap.set('n', '<leader><leader>h', builtin.help_tags, { desc = '[H]elp' })
-vim.keymap.set("n", "<leader><leader>tk", builtin.keymaps, {desc = 'telescope keymaps'})
+vim.keymap.set("n", "<leader><leader>tk", builtin.keymaps, { desc = 'telescope keymaps' })
 
-vim.keymap.set("n", "<leader>gb", builtin.git_branches, {desc = 'telescope git branchs'})
-vim.keymap.set("n", "<leader>glo", builtin.git_commits, {desc = 'git log'})
-vim.keymap.set("n", "<leader>glof", builtin.git_bcommits, {desc = 'git log current file'})
+vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = 'telescope git branchs' })
+vim.keymap.set("n", "<leader>glo", builtin.git_commits, { desc = 'git log' })
+vim.keymap.set("n", "<leader>glof", builtin.git_bcommits, { desc = 'git log current file' })
 
-vim.keymap.set("n", "<leader><leader>b", builtin.buffers, {desc = 'telescope buffers'})
-vim.keymap.set("n", "<leader><leader>p", project_files, {desc = 'Project File Search'})
-vim.keymap.set("n", "<leader><leader>f", function() builtin.grep_string({
+vim.keymap.set("n", "<leader><leader>b", builtin.buffers, { desc = 'telescope buffers' })
+vim.keymap.set("n", "<leader><leader>p", project_files, { desc = 'Project File Search' })
+vim.keymap.set("n", "<leader><leader>f", function()
+    builtin.grep_string({
         shorten_path = true,
         word_match = "-w",
         only_sort_text = true,
         search = '',
-    }, {desc = 'Project String Search'})
+    }, { desc = 'Project String Search' })
 end)
-
