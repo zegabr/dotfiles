b86eb0c334fc36c107e5c3ddd60990001f22addd
zegabr <jgsp2@cin.ufpe.br>
2023-03-22 01:44:54 -0300

    add starship

--- scripts/scripts/r.sh
+++ scripts/scripts/r.sh
@@ -4,4 +4,9 @@ source "$HOME/.cargo/env"
 rustc --version
 rustup component add rust-docs
 rustup component add rust-analyzer
-cargo install ripgrep exa bat
+cargo install ripgrep exa bat starship
+
+# if starship is not being started, add it to bashrc
+if [ -f ~/.bashrc ] && ! grep -q 'eval "$(starship init bash)"' ~/.bashrc; then
+   echo 'eval "$(starship init bash)"' >> ~/.bashrc
+fi
