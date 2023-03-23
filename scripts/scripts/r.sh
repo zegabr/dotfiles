d428bc036035b26beadcd54b49974d8ffaf090a3
zegabr <jgsp2@cin.ufpe.br>
2023-03-22 23:08:42 -0300

    remove zellij for now

--- scripts/scripts/r.sh
+++ scripts/scripts/r.sh
@@ -4,7 +4,8 @@ source "$HOME/.cargo/env"
 rustc --version
 rustup component add rust-docs
 rustup component add rust-analyzer
-cargo install ripgrep exa bat starship fd-find zellij
+cargo install ripgrep exa bat starship fd-find
+# cargo install zellij # TODO: waiting zellij new features
 
 # if starship is not being started, add it to bashrc
 if [ -f ~/.bashrc ] && ! grep -q 'eval "$(starship init bash)"' ~/.bashrc; then
