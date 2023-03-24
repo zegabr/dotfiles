1f177ecacc31ad6eca06857418ce8b87692daaf4
zegabr <jgsp2@cin.ufpe.br>
2023-03-23 22:11:30 -0300

    add more rust working tools

--- scripts/scripts/stuff/r.sh
+++ scripts/scripts/stuff/r.sh
@@ -4,7 +4,14 @@ source "$HOME/.cargo/env"
 rustc --version
 rustup component add rust-docs
 rustup component add rust-analyzer
-cargo install ripgrep exa bat starship fd-find speedtest-rs
+cargo install ripgrep \
+    exa \
+    bat \
+    starship \
+    fd-find \
+    gitui \
+    bacon \
+    --locked
 
 # if starship is not being started, add it to bashrc
 if [ -f ~/.bashrc ] && ! grep -q 'eval "$(starship init bash)"' ~/.bashrc; then
