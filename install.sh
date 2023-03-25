d50d54dd06267cb537b82ac0b219ead78df1face
zegabr <jgsp2@cin.ufpe.br>
2023-03-25 16:44:49 -0300

    remove scripts which are now functions

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # define a list of directories to be stowed
-directories=(scripts nvim bash bin terminal)
+directories=(scripts nvim bash terminal)
 
 # iterate over the list of directories
 for dir in ${directories[@]}
@@ -9,6 +9,3 @@ do
   # stow the directory in the home directory
   stow --verbose=2 -t ~ $dir
 done
-
-sudo chmod -x ~/.local/tmux-sessionizer.sh
-sudo chmod -x ~/.local/openai.sh
