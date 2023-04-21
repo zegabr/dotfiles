35e07c7812fa5d610b66c88b29ffe01e520671b8
zegabr <jgsp2@cin.ufpe.br>
2023-04-21 16:02:45 -0300

    add dinamic compose isntallation

--- scripts/scripts/setup_extra.sh
+++ scripts/scripts/setup_extra.sh
@@ -39,9 +39,11 @@ if [ "$ans" == "y" ]; then
     # sudo usermod -aG docker $USER # this is not in the installation page anymore?
 
     # from https://docs.docker.com/compose/install/other/
-    COMPOSE_VERSION=v2.16.0
+    COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
     sudo curl -L "https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+    sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
 fi
 
 # GOOGLE CHROME
