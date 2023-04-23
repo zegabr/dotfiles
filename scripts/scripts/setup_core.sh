a42e3cfa5a0e171928981ff61ed025774ead25a1
José Gabriel <jgsp2@cin.ufpe.br>
2023-04-23 18:40:08 -0300

    move rust installation

--- scripts/scripts/setup_core.sh
+++ scripts/scripts/setup_core.sh
@@ -22,6 +22,10 @@ neofetch \
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+# rust and cargo and cargo tools
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+gnome-terminal --wait -- bash -c "bash ~/dotfiles/scripts/scripts/stuff/r.sh" &
+
 # Cheat sheet
 curl https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh && sudo chmod +x /usr/local/bin/cht.sh
 
