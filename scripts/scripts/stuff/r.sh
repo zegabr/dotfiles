abe0111325cc2c2967b10baa9e5d03f2e95ad90b
José Gabriel <jgsp2@cin.ufpe.br>
2023-04-23 19:02:55 -0300

    Update r.sh

--- scripts/scripts/stuff/r.sh
+++ scripts/scripts/stuff/r.sh
@@ -17,10 +17,7 @@ cargo install \
     ripgrep \
     starship \
     bat \
-    exa \
-    gitui \
     speedtest-rs \
-    porsmo \
     --message-format short
 
 # if starship is not being started, add it to bashrc
@@ -31,16 +28,6 @@ fi
 
 if ! grep -q 'export STARSHIP_LOG=error' ~/.bashrc; then
     echo 'export STARSHIP_LOG=error' >> ~/.bashrc
-fi
-
-# check if RUSTC_WRAPPER is not an exported variable
-if ! grep -q 'export RUSTC_WRAPPER=~/.cargo/bin/sccache' ~/.bashrc; then
-    cargo install sccache
-    echo 'export RUSTC_WRAPPER=~/.cargo/bin/sccache' >> ~/.bashrc
-else
-    # get RUSTC_WRAPPER value and show to the user
-    echo "RUSTC_WRAPPER is already set to " "$RUSTC_WRAPPER"
-    echo "If you want to change it, please do it manually."
 fi
 
 
