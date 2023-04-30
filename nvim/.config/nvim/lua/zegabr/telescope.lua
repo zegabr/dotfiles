bb3eaf31bfc44a7d0673457da994716de655f557
zegabr <jgsp2@cin.ufpe.br>
2023-04-30 14:08:18 -0300

    use rg defaults for telescope

--- nvim/.config/nvim/lua/zegabr/telescope.lua
+++ nvim/.config/nvim/lua/zegabr/telescope.lua
@@ -22,17 +22,6 @@ local mymappings = {
 telescope.setup {
     defaults = {
         mappings = { i = mymappings, n = mymappings, },
-        vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-            "--trim",
-            "--hidden",
-        },
         file_ignore_patterns = {
             "node_modules/",
             ".git/",
