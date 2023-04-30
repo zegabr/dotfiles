a94c2e0bc04c459cbdb9a942aac9ef170e8f504b
zegabr <jgsp2@cin.ufpe.br>
2023-04-30 14:17:49 -0300

    update scripts to use subscripts

--- scripts/scripts/stuff/d.sh
+++ scripts/scripts/stuff/d.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-CURR_DIR=$(pwd)
+#
+(
 cd ~/dotfiles/
 source install.sh
-cd $CURR_DIR
+)
--- scripts/scripts/stuff/v.sh
+++ scripts/scripts/stuff/v.sh
@@ -1,6 +1,6 @@
 # v for (neo)vim
 
-CURR_DIR=$(pwd)
+(
 # dependencies
 sudo apt update && sudo apt upgrade -y
 sudo apt install ninja-build gettext libtool-bin cmake g++ pkg-config unzip xsel -y
@@ -13,8 +13,7 @@ git clone https://github.com/neovim/neovim
 cd neovim && rm -rf build && make distclean && git pull
 # build and install
 make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install
-
-cd $CURR_DIR
+)
 
 # python plugin dependencies
 python3 -m pip install --user --upgrade pynvim
