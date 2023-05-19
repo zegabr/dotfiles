df27c7a48a3990e154816c16a232f24ed5e5a8a2
zegabr <jjgabrielsp@gmail.com>
2023-05-19 14:11:46 -0300

    add wsl setup script

--- scripts/scripts/wsl.sh
+++ scripts/scripts/wsl.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-# m for main
+# wsl
 
 # core
 bash ~/dotfiles/scripts/scripts/setup_core.sh
-
-# extra
-bash ~/dotfiles/scripts/scripts/setup_extra.sh
 
 # setup ssh for github
 bash ~/dotfiles/scripts/scripts/stuff/ssh.sh
