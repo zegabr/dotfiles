d563d0d2b09b37108a225ee77db207daeaac40e5
zegabr <jjgabrielsp@gmail.com>
2023-05-24 15:39:00 -0300

    testing interactive script

--- scripts/scripts/m.sh
+++ scripts/scripts/m.sh
@@ -7,7 +7,7 @@ bash ~/dotfiles/scripts/scripts/setup_core.sh
 # extra
 bash ~/dotfiles/scripts/scripts/setup_extra.sh
 
-# setup ssh for github
+# setup ssh for github TODO: add this to readme so I remember how to run it before everything using curl | bash
 bash ~/dotfiles/scripts/scripts/stuff/ssh.sh
 
 # pnpm for nvim lsp
