2d335b6d8f517493d4879dc1da37acf84ce98732
zegabr <jjgabrielsp@gmail.com>
2023-05-25 15:59:42 -0300

    simplfy my life

--- README.md
+++ README.md
@@ -5,12 +5,17 @@ my dotfiles manager
 ## What it has?
 - neovim (as a fully featured IDE) + tmux + rust useful CLIs
 
-## if you want an adventure, here is how to use it
-- For ssh key setup + core features like tmux, nvim, and CLI tools, run:
-    - `bash <(curl -sSL https://raw.githubusercontent.com/zegabr/dotfiles/main/scripts/scripts/stuff/ssh.sh)`
-    - `bash <(curl -sSL https://raw.githubusercontent.com/zegabr/dotfiles/main/scripts/scripts/setup_core.sh)`
-- For other desktop applications on Debian distros, try:
-    - `bash <(curl -sSL https://raw.githubusercontent.com/zegabr/dotfiles/main/scripts/scripts/setup_extra.ss`
+## If you want an adventure, here is how to use it
+### For ssh key setup 
+`bash <(curl -sSL https://raw.githubusercontent.com/zegabr/dotfiles/main/scripts/scripts/stuff/ssh.sh)`
+
+###  For core features like tmux, nvim, and CLI tools,
+`git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install`
+`sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash`
+`bash <(curl -sSL https://raw.githubusercontent.com/zegabr/dotfiles/main/scripts/scripts/setup_core.sh)`
+
+### For extra desktop applications on debian distros
+`bash <(curl -sSL https://raw.githubusercontent.com/zegabr/dotfiles/main/scripts/scripts/setup_extra.ss`
 
 ## TODO:
 - learn how to use and setup debbuger for languages like go, rust, and maybe typescript for neovim
--- scripts/scripts/setup_core.sh
+++ scripts/scripts/setup_core.sh
@@ -3,44 +3,38 @@
 mkdir ~/Downloads
 cd ~/Downloads
 
-# sudo apt update -y
+sudo apt update -y
 
-# sudo apt install \
-# git \
-# stow \
-# tree \
-# wget \
-# curl \
-# gawk \
-# htop \
-# jq \
-# python3-pip \
-# neofetch \
-# -y
+sudo apt install \
+git \
+stow \
+tree \
+wget \
+curl \
+gawk \
+htop \
+jq \
+python3-pip \
+neofetch \
+-y
 
-# sudo apt install python3-pip -y
-# pip install repren
-
-# FZF with install script
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
-
-# rust and cargo and cargo tools
-sudo bash <(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)
+sudo apt install python3-pip -y
+pip install repren
 
 # Cheat sheet
-# curl https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh && sudo chmod +x /usr/local/bin/cht.sh
+curl https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh && sudo chmod +x /usr/local/bin/cht.sh
 
 # tmux
-# sudo apt install libevent-dev libncurses-dev build-essential xclip tmux -y
+sudo apt install libevent-dev libncurses-dev build-essential xclip tmux -y
 
 # install nvim building from source
-# curl -sSL https://raw.githubusercontent.com/zegabr/dotfiles/main/scripts/scripts/stuff/v.sh | bash
+curl -sSL https://raw.githubusercontent.com/zegabr/dotfiles/main/scripts/scripts/stuff/v.sh | bash
 
 # pnpm for nvim lsp
-# curl -sSL https://raw.githubusercontent.com/zegabr/dotfiles/main/scripts/scripts/stuff/n.sh | bash
+curl -sSL https://raw.githubusercontent.com/zegabr/dotfiles/main/scripts/scripts/stuff/n.sh | bash
 
 # simlinks dotfiles
-# curl -sSL https://raw.githubusercontent.com/zegabr/dotfiles/main/scripts/scripts/stuff/d.sh | bash
+curl -sSL https://raw.githubusercontent.com/zegabr/dotfiles/main/scripts/scripts/stuff/d.sh | bash
 
 #for work note taking
 mkdir ~/notes
