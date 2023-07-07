e543c6b41da9ca4d7be9a3eebdbef2bb42a410c8
zegabr <jjgabrielsp@gmail.com>
2023-07-07 10:11:17 -0300

    update windows.bat

--- scripts/scripts/stuff/windows.bat
+++ scripts/scripts/stuff/windows.bat
@@ -1,20 +1,20 @@
-winget install PowerShell --source msstore --accept-source-agreements --accept-package-agreements
+REM winget install PowerShell --source msstore --accept-source-agreements --accept-package-agreements
+
 winget install Telegram.TelegramDesktop --accept-source-agreements --accept-package-agreements
 winget install Spotify --source msstore --accept-source-agreements --accept-package-agreements
-winget install TeamViewer --source msstore --accept-source-agreements --accept-package-agreements
 winget install Discord.Discord --accept-source-agreements --accept-package-agreements
 winget install Epicgames.epicgameslauncher --accept-source-agreements --accept-package-agreements
-winget install OBS --source msstore --accept-source-agreements --accept-package-agreements
-winget install ubuntu --source msstore --accept-source-agreements --accept-package-agreements
 winget install Google.Chrome --accept-source-agreements --accept-package-agreements
 winget install Valve.Steam --accept-source-agreements --accept-package-agreements
-winget install Microsoft.VisualStudioCode --accept-source-agreements --accept-package-agreements
 winget install qBittorrent.qBittorrent --accept-source-agreements --accept-package-agreements
 winget install VideoLAN.VLC --accept-source-agreements --accept-package-agreements
 winget install Thedocumentfoundation.libreoffice --accept-source-agreements --accept-package-agreements
 winget install Brave.Brave --accept-source-agreements --accept-package-agreements
 
-
+REM winget install Microsoft.VisualStudioCode --accept-source-agreements --accept-package-agreements
+REM winget install TeamViewer --source msstore --accept-source-agreements --accept-package-agreements
+REM winget install OBS --source msstore --accept-source-agreements --accept-package-agreements
+REM winget install ubuntu --source msstore --accept-source-agreements --accept-package-agreements
 
 
 
