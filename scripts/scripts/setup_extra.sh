fe8d337f0fbddc219fa5292554d1d60ed6d79ae3
José Gabriel <jgsp2@cin.ufpe.br>
2023-09-07 17:49:21 -0300

    Update setup_extra.sh

--- scripts/scripts/setup_extra.sh
+++ scripts/scripts/setup_extra.sh
@@ -65,6 +65,7 @@ fi
 if [ "$SPOTIFY" == 1 ]; then
     curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
     echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+    sudo apt update -y
     sudo apt install spotify-client -y
 fi
 
