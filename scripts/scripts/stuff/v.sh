1e405c78fd85041569102ca24ff5b1e9ae5f06bd
zegabr <jjgabrielsp@gmail.com>
2023-09-15 20:42:12 -0300

    downgrade nvim to fix telescope issue, for now

--- scripts/scripts/stuff/v.sh
+++ scripts/scripts/stuff/v.sh
@@ -1,4 +1,4 @@
-# v for (neo)vim
+# v for (neo)vim TODO: yo udon't need this if the package manager has neovim 0.9.x
 
 (
 # dependencies
@@ -11,6 +11,8 @@ mkdir ~/nvim-deps
 cd ~/nvim-deps
 git clone https://github.com/neovim/neovim
 cd neovim && rm -rf build && make distclean && git pull
+# temporary fix for telescope TODO: try removing this after next release
+git checkout b641fc3
 # build and install
 make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install
 )
