e8c09a7539d283a2770afa5c5a689cfc314bf862
José Gabriel <jjgabrielsp@gmail.com>
2023-12-02 23:28:43 -0300

    Update wsl.sh

--- scripts/scripts/wsl.sh
+++ scripts/scripts/wsl.sh
@@ -4,7 +4,7 @@
 
 # for windows WSL neovim to work with clipboard
 sudo apt update -y
-sudo apt install xdg-utils -y
+sudo apt install xdg-utils curl unzip -y
 sudo curl -sLo/tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
 sudo unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
 sudo chmod +x /tmp/win32yank.exe
