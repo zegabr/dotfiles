45a9a47703fa51b3af29d1fcd74ed4cfa88d6bcf
José Gabriel <jjgabrielsp@gmail.com>
2023-12-08 21:44:55 -0300

    Update windows.bat

--- scripts/scripts/stuff/windows.bat
+++ scripts/scripts/stuff/windows.bat
@@ -1,4 +1,4 @@
-REM winget install PowerShell --source msstore --accept-source-agreements --accept-package-agreements
+winget install PowerShell --source msstore --accept-source-agreements --accept-package-agreements
 
 winget install Telegram.TelegramDesktop --accept-source-agreements --accept-package-agreements
 winget install Spotify --source msstore --accept-source-agreements --accept-package-agreements
@@ -11,10 +11,10 @@ winget install VideoLAN.VLC --accept-source-agreements --accept-package-agreemen
 winget install Thedocumentfoundation.libreoffice --accept-source-agreements --accept-package-agreements
 winget install Brave.Brave --accept-source-agreements --accept-package-agreements
 
-REM winget install Microsoft.VisualStudioCode --accept-source-agreements --accept-package-agreements
-REM winget install TeamViewer --source msstore --accept-source-agreements --accept-package-agreements
-REM winget install OBS --source msstore --accept-source-agreements --accept-package-agreements
-REM winget install ubuntu --source msstore --accept-source-agreements --accept-package-agreements
+winget install Microsoft.VisualStudioCode --accept-source-agreements --accept-package-agreements
+winget install TeamViewer --source msstore --accept-source-agreements --accept-package-agreements
+winget install OBS --source msstore --accept-source-agreements --accept-package-agreements
+winget install ubuntu --source msstore --accept-source-agreements --accept-package-agreements
 
 
 
