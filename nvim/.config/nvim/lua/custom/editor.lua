7aaa486796dac6fda0b8657f4ffef797f44cddd1
zegabr <jjgabrielsp@gmail.com>
2023-12-13 16:02:27 -0300

    add hidden files to oil
    Signed-off-by: zegabr <jjgabrielsp@gmail.com>

--- nvim/.config/nvim/lua/custom/editor.lua
+++ nvim/.config/nvim/lua/custom/editor.lua
@@ -45,8 +45,22 @@ return {
     {
         'stevearc/oil.nvim',
         config = function()
-            require("oil").setup()
-            vim.keymap.set("n", "<Leader>e", ":Oil<CR>", {desc = "open file tree"})
+            require("oil").setup({
+                view_options = {
+                    show_hidden = true,
+                },
+                delete_to_trash = true,
+                use_default_keymaps = true,
+                keymaps = {
+                    ["g?"] = "actions.show_help",
+                    ["<CR>"] = "actions.select",
+                    ["<C-c>"] = "actions.close",
+                    ["-"] = "actions.parent",
+                    ["_"] = "actions.open_cwd",
+                    ["g."] = "actions.toggle_hidden",
+                },
+            })
+            vim.keymap.set("n", "<Leader>e", ":Oil<CR>", { desc = "open file tree" })
         end
     },
 }
