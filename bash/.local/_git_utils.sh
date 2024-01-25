570f52910731067df14554486eea048aeaec6248
zegabr <jjgabrielsp@gmail.com>
2024-01-25 19:15:56 -0300

    fix lg alias

--- bash/.local/_git_utils.sh
+++ bash/.local/_git_utils.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-alias lg='lazygit'https://www.youtube.com/watch?v=TLNdwvIFoCg&list=WL&index=10
+alias lg='lazygit'
 alias gs='git status'
 alias guncommit='git reset HEAD~1 --soft'
 function _ga_completion() {
