db4ffeadb5d99b55af615456fbf6cf3a1945bd6a
zegabr <jjgabrielsp@gmail.com>
2024-01-29 11:51:18 -0300

    git specific plugins on git repos only

--- nvim/.config/nvim/lua/custom/git.lua
+++ nvim/.config/nvim/lua/custom/git.lua
@@ -1,8 +1,15 @@
+local is_git_repo = function()
+    local cwd = vim.fn.getcwd()
+    local git_dir = vim.fn.expand(cwd .. '/.git')
+    return vim.fn.isdirectory(git_dir) == 1
+end
+
 return { -- specific plugins to work with git, doesn't allow mercurial
 
     {
         'ruifm/gitlinker.nvim', -- get premalink by <leader>gy (works in visuali mode)
         keys = '<leader>gy',
+        enabled = is_git_repo(),
         config = function()
             require("gitlinker").setup({
                 mappings = "<leader>gy",
@@ -13,6 +20,7 @@ return { -- specific plugins to work with git, doesn't allow mercurial
     {
         'f-person/git-blame.nvim',
         cmd = 'GitBlameToggle',
+        enabled = is_git_repo(),
         config = function()
             require('gitblame').setup({ enabled = false, })
             -- :GitBlameToggle when needed
