3aaf7d46aeb095b41ae33acd8e0ac87a2ab3fa20
José Gabriel Silva Pereira <zezin@google.com>
2025-06-13 14:31:11 -0300

    fix jump options behaviour
    Signed-off-by: José Gabriel Silva Pereira <zezin@google.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -89,6 +89,7 @@ vim.opt.cursorcolumn = true
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "80"
 vim.opt.background = "dark"
+vim.g.jumpoptions = "stack"
 vim.cmd [[
       set guioptions-=e " Use showtabline in gui vim
       set sessionoptions+=tabpages,globals " store tabpages and globals in session
