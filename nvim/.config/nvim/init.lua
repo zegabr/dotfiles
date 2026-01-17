f3ac5f2695cdc5595effac92493f088f5b2ddfe0
José Gabriel Silva Pereira <zezin@google.com>
2026-01-16 23:23:06 -0300

    Disable treesitter
    Signed-off-by: José Gabriel Silva Pereira <zezin@google.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -184,58 +184,58 @@ local plugins = {
         },
     },
 
-    { -- Treesitter
-        'nvim-treesitter/nvim-treesitter',
-        event = { 'BufReadPre', 'BufNewFile' },
-        build = function()
-            require('nvim-treesitter.install').update({ with_sync = true })()
-        end,
-        config = function()
-            require('nvim-treesitter.configs').setup {
-                ensure_installed = {
-                    -- essential
-                    "bash", "comment", "lua", "luadoc", "vim", "vimdoc", "diff", "gitcommit", "markdown", "markdown_inline", "xml", "yaml",
-                    -- most used
-                    "javascript", "typescript", "go", "c", "cpp", "python", "kotlin", "java", "html", "jsdoc", "rust", "sql", "tsx",
-                },
-                sync_install = false,
-                -- auto_install = true, -- works since i will have treesitter cli installed
-                indent = { enable = true },
-                highlight = { enable = true, },
-                -- textobjects = {
-                --     enable = true,
-                --     select = {
-                --         enable = true,
-                --         -- Automatically jump forward to textobj, similar to targets.vim
-                --         lookahead = true,
-                --         keymaps = {
-                --             -- You can use the capture groups defined in textobjects.scm
-                --             ["af"] = "@function.outer",
-                --             ["if"] = "@function.inner",
-                --             ["ac"] = "@class.outer",
-                --             ["ic"] = "@class.inner",
-                --         },
-                --     },
-                --     move = {
-                --         enable = true,
-                --         set_jumps = true,
-                --         goto_next_start = {
-                --             [']f'] = '@function.outer',
-                --             [']c'] = '@class.outer'
-                --         },
-                --         goto_previous_start = {
-                --             ['[f'] = '@function.outer',
-                --             ['[c'] = '@class.outer'
-                --         },
-                --     },
-                --     swap = {
-                --         enable = true,
-                --         swap_next = { ["<leader>sw"] = "@parameter.inner" },
-                --     }
-                -- }
-            }
-        end,
-    },
+    -- { -- Treesitter
+    --     'nvim-treesitter/nvim-treesitter',
+    --     event = { 'BufReadPre', 'BufNewFile' },
+    --     build = function()
+    --         require('nvim-treesitter.install').update({ with_sync = true })()
+    --     end,
+    --     config = function()
+    --         require('nvim-treesitter.configs').setup {
+    --             ensure_installed = {
+    --                 -- essential
+    --                 "bash", "comment", "lua", "luadoc", "vim", "vimdoc", "diff", "gitcommit", "markdown", "markdown_inline", "xml", "yaml",
+    --                 -- most used
+    --                 "javascript", "typescript", "go", "c", "cpp", "python", "kotlin", "java", "html", "jsdoc", "rust", "sql", "tsx",
+    --             },
+    --             sync_install = false,
+    --             -- auto_install = true, -- works since i will have treesitter cli installed
+    --             indent = { enable = true },
+    --             highlight = { enable = true, },
+    --             -- textobjects = {
+    --             --     enable = true,
+    --             --     select = {
+    --             --         enable = true,
+    --             --         -- Automatically jump forward to textobj, similar to targets.vim
+    --             --         lookahead = true,
+    --             --         keymaps = {
+    --             --             -- You can use the capture groups defined in textobjects.scm
+    --             --             ["af"] = "@function.outer",
+    --             --             ["if"] = "@function.inner",
+    --             --             ["ac"] = "@class.outer",
+    --             --             ["ic"] = "@class.inner",
+    --             --         },
+    --             --     },
+    --             --     move = {
+    --             --         enable = true,
+    --             --         set_jumps = true,
+    --             --         goto_next_start = {
+    --             --             [']f'] = '@function.outer',
+    --             --             [']c'] = '@class.outer'
+    --             --         },
+    --             --         goto_previous_start = {
+    --             --             ['[f'] = '@function.outer',
+    --             --             ['[c'] = '@class.outer'
+    --             --         },
+    --             --     },
+    --             --     swap = {
+    --             --         enable = true,
+    --             --         swap_next = { ["<leader>sw"] = "@parameter.inner" },
+    --             --     }
+    --             -- }
+    --         }
+    --     end,
+    -- },
 
 
 
@@ -678,20 +678,6 @@ local plugins = {
                     lspconfig( server_name, server_opts)
                 end
             end,
-        },
-
-        { -- annotation/comment doc generator -- USAGE: `:Neogen`
-            "danymat/neogen",
-            dependencies = {
-                "nvim-treesitter/nvim-treesitter",
-                "L3MON4D3/LuaSnip",
-            },
-            config = function()
-                require("neogen").setup({
-                    snippet_engine = "luasnip"
-                })
-            end,
-            version = "*",
         },
     },
 
